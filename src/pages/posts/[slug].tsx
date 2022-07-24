b0afad309dd41293a81ff580109a8598d9e776a9
Thonny Noda <noda.thonny@gmail.com>
2022-07-24 10:50:16 -0300

    finished project

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -46,7 +46,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const { slug } = params;
   console.log(session);
 
-if(!session.activeSubscription) {
+if(!session?.activeSubscription) {
   return{
     redirect:{
       destination:'/',
